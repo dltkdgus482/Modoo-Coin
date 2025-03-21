79478cd0964eae72e5545ed96d050bcf0ac6e1a4
unknown <abs3011@konkuk.ac.kr>
2025-03-21 11:27:56 +0900

    feat: 히스토리 버튼 및 모달창 구현

--- src/components/position/PositionHeader.jsx
+++ src/components/position/PositionHeader.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 // Libraries
 import styled from 'styled-components';
 
-const PositionHeader = ({ setPositionArray }) => {
+// Modal
+import HistoryModal from '../modal/HistoryModal.jsx';
+
+const PositionHeader = ({tradeDataHistory}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsModalOpen(true); // 모달 열기
+  };
+
   return (
     <Container>
       <Header>My Positions</Header>
+      <ClearButton onClick={handleClick}>My History</ClearButton>
+      {isModalOpen && <HistoryModal onClose={() => setIsModalOpen(false)} tradeDataHistory={tradeDataHistory} />}
     </Container>
   );
 };
@@ -18,10 +31,29 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
 `;
 
 const Header = styled.p`
   color: #008485;
   font-size: 120%;
   font-weight: bold;
+  font-family: 'Press Start 2P', 'Pixelify Sans', monospace;
+`;
+
+const ClearButton = styled.button`
+  padding: 8px 15px;
+  font-size: 12px;
+  font-weight: bold;
+  font-family: 'Press Start 2P', 'Pixelify Sans', monospace;
+  background-color: #008485;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+
+  &:hover {
+    background-color: #005f5f;
+  }
 `;
